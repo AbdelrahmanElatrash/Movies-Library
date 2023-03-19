a49bcf17dcdd930dc70b4af2578000f7e7ae2e43
AbdelrahmanElatrash <a.elatrash170@gmail.com>
2023-03-19 12:56:42 +0000

    connected with front end befor deployment

--- router/movies_db.js
+++ router/movies_db.js
@@ -48,15 +48,17 @@ router.post('/',(req,res,next)=>{
     let release_date=data.relaseDate;
     let posterPath=data.posterPath;
     let overview=data.overVewo;
+    let commint=data.commint ||"" ;
     
-    const sql=`INSERT INTO movies (title,release_date,posterPath,overview) VALUES($1,$2,$3,$4)RETURNING *;`
-    const values = [title,release_date,posterPath,overview];
+    const sql=`INSERT INTO movies (title,release_date,posterPath,overview,commint) VALUES($1,$2,$3,$4,$5)RETURNING *;`
+    const values = [title,release_date,posterPath,overview,commint];
     client.query(sql,values)
     .then((data)=>{
         res.status(200).json({"data":{"title":title,
                                       "release_date":release_date,
                                       "posterPath":posterPath,
-                                      "overview":overview
+                                      "overview":overview ,
+                                      "commint": commint
                                       }
 
 })
@@ -88,14 +90,20 @@ router.delete('/:id',(req,res,next)=>{
 router.put('/:id',(req,res,next)=>{
     let id=req.params.id ;
     let data=req.body;
-    console.log(data);
+    console.log(id);
+    console.log(data.id)
+    
     let title=data.title;
     let release_date=data.relaseDate;
-    let posterPath=data.posterPath;
-    let overview=data.overVewo;
+    let posterPath=data.posterPath
+    let overview=data.overVewo
+    let commint=data.commint
 
-    const sql=`UPDATE movies SET title='${title}',release_date='${release_date}',posterPath='${posterPath}',overview='${overview}' WHERE id=${id} ;`;
-    client.query(sql)
+    
+   
+    const sql=`UPDATE movies SET title=$1,release_date=$2,posterPath=$3,overview=$4,commint=$5 WHERE id=${id} RETURNING * ;`;
+    const values = [title,release_date,posterPath,overview,commint];
+    client.query(sql,values)
     .then((data)=>{
         res.status(200).json({"data":{"title":title,
         "release_date":release_date,
